2e4494b13627650d24fc41fe13dbe4ffdb735761
Zeno Sbardelotto Junior <zeno.sbardelotto@universo.univates.br>
2020-08-05 20:27:45 -0300

    Logo, .env, test heroku

--- api/database/migrations/1503250034279_user.js
+++ api/database/migrations/1503250034279_user.js
@@ -8,6 +8,7 @@ class UserSchema extends Schema {
     this.create('users', (table) => {
       table.increments()
       table.string('username', 80).notNullable().unique()
+      table.string('name', 120).notNullable()
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
       table.timestamps()
