eae53977d27dc716c3293d7ea31ef59e238f6382
Zeno Sbardelotto Junior <zeno.sbardelotto@universo.univates.br>
2020-08-05 20:52:54 -0300

    Heroku test buildpack 03 (html)

--- api/start/routes.js
+++ api/start/routes.js
@@ -17,7 +17,42 @@
 const Route = use('Route')
 
 Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
+
+  let html = `
+  <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Trellu</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+      </head>
+      <body classes="assa">
+        <h1>Trellu</h1>
+        <script>
+          window.fbAsyncInit = function() {
+            FB.init({
+              appId      : '{your-app-id}',
+              cookie     : true,
+              xfbml      : true,
+              version    : '{api-version}'
+            });
+              
+            FB.AppEvents.logPageView();   
+              
+          };
+          (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+          }(document, 'script', 'facebook-jssdk'));
+        </script>
+      </body>
+    </html>
+  `;
+
+  return html;
 })
 Route.get('login/facebook', 'UserController.redirect')
 Route.get('facebook/callback', 'UserController.callback')
