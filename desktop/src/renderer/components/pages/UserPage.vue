bdbe40522e949c2c486c753bfa63482441ad9f11
cris-scheib <cris.scheib@hotmail.com>
2020-08-11 21:40:06 -0300

    Criação parcial da tela de usuários

--- desktop/src/renderer/components/pages/UserPage.vue
+++ desktop/src/renderer/components/pages/UserPage.vue
@@ -2,8 +2,18 @@
   <div>
     <navbar></navbar>
     <div class="container is-fluid pt-6">
-      <h1>User page</h1>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus consectetur enim cupiditate atque perspiciatis ullam molestias neque maiores nemo, unde reiciendis autem consequuntur culpa eos voluptates incidunt qui quidem a!</p>
+      <div class="columns">
+        <div class="column is-two-fifths center">
+          <img src="~@/assets/user.png" class="user-pick" alt="User picture">
+        </div>
+        <div class="column">
+            <div class="user-info">
+                <h2 class="user-name">{{user.name}}</h2>
+                <h3 class="user-username">{{user.username}}</h3>
+                <h3 class="user-email">{{user.email}}</h3>
+            </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -11,6 +21,14 @@
 <script>
 import Navbar from '../Navbar'
 export default {
+  data () {
+    return {
+      user: {}
+    }
+  },
+  created () {
+    this.user = JSON.parse(localStorage.getItem('user'))
+  },
   components: {
     Navbar
   }
@@ -19,6 +37,27 @@ export default {
 
 <style lang="scss" scoped>
 .container {
-  padding-top: 1rem;
+    padding-top: 1rem;
+}
+.user-pick{
+    background: linear-gradient(45deg, #0079bf, #89609e);
+    border-radius: 100%;
+    margin-top: 2em;
+    width: 60%;
+}
+.user-name{
+    font-size: 2.5em;
+    text-transform: capitalize;
+}
+.user-info{
+    margin-top: 3em;
+}
+.user-username, .user-email{
+    font-size: 1.5em;
+}
+.center{
+    justify-content: center;
+    align-items: center;
+    display: flex;
 }
 </style>
