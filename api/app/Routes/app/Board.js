165f819d6c0228f976ab57fdca425cc45e975a54
cris-scheib <cris.scheib@hotmail.com>
2020-09-12 00:13:31 -0300

    Update on routes app and admin

--- api/app/Routes/app/Board.js
+++ api/app/Routes/app/Board.js
@@ -3,10 +3,10 @@
 const Route = use('Route')
 
 module.exports = () => {
-  Route.get('/', 'BoardController.boards').middleware('auth')
-  Route.post('/', 'BoardController.create').validator('boards/Create').middleware('auth')
-  Route.get('/:id', 'BoardController.board').middleware('auth')
-  Route.put('/:id', 'BoardController.update').middleware('auth')
-  Route.delete('/:id', 'BoardController.delete').middleware('auth')
-  Route.post('/:id/ordenate', 'BoardController.ordenate').middleware('auth')
+  Route.get('/', 'app/BoardController.boards').middleware('auth')
+  Route.post('/', 'app/BoardController.create').validator('boards/Create').middleware('auth')
+  Route.get('/:id', 'app/BoardController.board').middleware('auth')
+  Route.put('/:id', 'app/BoardController.update').middleware('auth')
+  Route.delete('/:id', 'app/BoardController.delete').middleware('auth')
+  Route.post('/:id/ordenate', 'app/BoardController.ordenate').middleware('auth')
 }
