f9be7a0fba38f52bd196adb2330e9d560e30fd86
cris-scheib <cris.scheib@hotmail.com>
2020-09-12 00:14:46 -0300

    Creation of the user groups

--- api/start/socket.js
+++ api/start/socket.js
@@ -1,7 +1,7 @@
-const Server = use('Server')
-const io = use('socket.io')(Server.getInstance())
+// const Server = use('Server')
+// const io = use('socket.io')(Server.getInstance())
 
-io.on('connection', function (socket) {
+// io.on('connection', function (socket) {
   // let cookie = socket.handshake.headers.cookie;
-  console.log(`a user connected ${socket.id}`);
-})
+  // console.log(`a user connected ${socket.id}`);
+// })
