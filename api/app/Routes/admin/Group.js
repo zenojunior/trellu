51c4048f0013aec0881d2ea21ec41eb2c35f0199
Zeno Junior <zeno.sbardelotto@universo.univates.br>
2020-09-13 00:39:13 -0300

    Groups Users

--- api/app/Routes/admin/Group.js
+++ api/app/Routes/admin/Group.js
@@ -4,6 +4,7 @@ const Route = use('Route')
 
 module.exports = () => {
   Route.get('/', 'admin/GroupController.groups').middleware('auth')
+  Route.get('/:id', 'admin/GroupController.group').middleware('auth')
   Route.post('/', 'admin/GroupController.create').middleware('auth')
   Route.put('/:id', 'admin/GroupController.update').middleware('auth')
   Route.delete('/:id', 'admin/GroupController.delete').middleware('auth')
