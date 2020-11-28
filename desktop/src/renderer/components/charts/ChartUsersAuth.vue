d2dd183c3ee094a3bb12db5c104d78046c72638c
Zeno Junior <zeno.sbardelotto@universo.univates.br>
2020-11-28 18:55:45 -0300

    Charts automatic get months

--- desktop/src/renderer/components/charts/ChartUsersAuth.vue
+++ desktop/src/renderer/components/charts/ChartUsersAuth.vue
@@ -1,6 +1,9 @@
 <template>
   <article class="panel is-info">
-    <p v-if="title" class="panel-heading">{{ title }}</p>
+    <div class="panel-heading">
+      <p class="panel-title" v-if="title">{{ title }}</p>
+      <b-numberinput size="is-small" v-model="limit" :min="3" :max="8" @input="getData()" controls-position="compact" controls-rounded></b-numberinput>
+    </div>
     <div class="panel-block-light">
       <chart width="100%" type="bar" :options="options" :series="series" />
     </div>
@@ -22,10 +25,12 @@ export default {
           id: 'vuechart-example'
         },
         xaxis: {
-          categories: ['Ago', 'Set', 'Out', `Nov`]
+          categories: []
         }
       },
-      series: []
+      series: [],
+      months: [],
+      limit: 4
     }
   },
   created () {
@@ -33,7 +38,16 @@ export default {
   },
   methods: {
     async getData () {
-      const {login, logout} = await this.$api.post('/api/admin/dashboard/logins-logouts', {months: [8, 9, 10, 11]}).then(res => res.data)
+      this.months = this.getLastMonths(this.limit)
+      this.options = {
+        chart: {
+          id: 'vuechart-example'
+        },
+        xaxis: {
+          categories: [...this.getMonthName(this.months)]
+        }
+      }
+      const {login, logout} = await this.$api.post('/api/admin/dashboard/logins-logouts', {months: this.months}).then(res => res.data)
       this.series = [
         {
           name: 'Logins',
@@ -44,6 +58,32 @@ export default {
           data: logout.map((value) => parseInt(value))
         }
       ]
+    },
+    getMonthName (months) {
+      let names = {
+        1: 'Jan',
+        2: 'Fev',
+        3: 'Mar',
+        4: 'Abr',
+        5: 'Mai',
+        6: 'Jun',
+        7: 'Jul',
+        8: 'Ago',
+        9: 'Set',
+        10: 'Out',
+        11: 'Nov',
+        12: 'Dez'
+      }
+      return months.map(mm => names[mm]).reverse()
+    },
+    getLastMonths (total) {
+      let months = []
+      const getMonth = () => new Date().getMonth() + 1
+      months.push(getMonth())
+      for (let i = 1; i < total; i++) {
+        months.push(getMonth() - i)
+      }
+      return months
     }
   }
 }
@@ -55,5 +95,15 @@ export default {
   border-left: 1px solid #dbdbdb;
   border-right: 1px solid #dbdbdb;
   padding-top: 5px;
+}
+.panel-heading {
+  display: flex;
+  .panel-title {
+    flex: 1;
+  }
+  .panel-field {
+    margin-left: auto;
+    flex: 1;
+  }
 }
 </style>
